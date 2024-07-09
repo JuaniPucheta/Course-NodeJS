2c9ce4504b5ea5f833c7110eb13a8d99a68a57fc
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-07-09 16:30:19 -0300

    modules systems

---
+++ 01 Introduccion y primeros pasos/cjs/index.js
@@ -0,0 +1,9 @@
+globalThis.miVariable = 'el valor de mi variable';
+//? globalThis
+    //* es una variable global en toda nuestra aplicacion
+
+
+// CommonJS require module
+const { sum } = require('./sum');
+console.log(sum(1, 2));
+
