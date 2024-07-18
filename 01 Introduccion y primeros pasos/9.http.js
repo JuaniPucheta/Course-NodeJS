38dfad629a9a8c5b8e9108f0fd4e8f8cb97765eb
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-07-17 21:42:36 -0300

    free port

--- 01 Introduccion y primeros pasos/9.http.js
+++ 01 Introduccion y primeros pasos/9.http.js
@@ -1,10 +1,13 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./10.free-port')
 
 const server = http.createServer((req, res) => {
   console.log('Nueva petición')
   res.end('Hola mundo')
 })
 
-server.listen(0, () => {
-  console.log(`Servidor escuchando en http://localhost:${server.address().port}`)
+findAvailablePort(3000).then(port => {
+  server.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`)
+  })
 })
