3bc9ae93818a02d8d4edffdd979f41e09bdcc7e2
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-07-17 21:44:42 -0300

    desiredPort

--- 01 Introduccion y primeros pasos/9.http.js
+++ 01 Introduccion y primeros pasos/9.http.js
@@ -1,12 +1,15 @@
 const http = require('node:http')
 const { findAvailablePort } = require('./10.free-port')
 
+const desiredPort = process.env.PORT ?? 3000
+// PORT=3001 node 9.http.js
+
 const server = http.createServer((req, res) => {
   console.log('Nueva petición')
   res.end('Hola mundo')
 })
 
-findAvailablePort(3000).then(port => {
+findAvailablePort(desiredPort).then(port => {
   server.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`)
   })
