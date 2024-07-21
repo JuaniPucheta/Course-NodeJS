88e8bf3c97be2432934db63047f47f2bb248c1fa
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-07-21 18:18:21 -0300

    3.express with middleware (commented below)

--- 02 API con Express desde cero/3.express.js
+++ 02 API con Express desde cero/3.express.js
@@ -6,27 +6,31 @@ const PORT = process.env.PORT ?? 3000
 const app = express()
 app.disable('x-powered-by')
 
-app.use((req, res, next) => {
-    if (req.method !== 'POST') return next()
-    if (req.headers['content-type'] !== 'application/json') return next()
+// * esto hace exactamente lo mismo que lo de abajo
+app.use(express.json())
+/*
+    app.use((req, res, next) => {
+        if (req.method !== 'POST') return next()
+        if (req.headers['content-type'] !== 'application/json') return next()
 
-    // solo llegan request que son POST y que tienen el header content-type: application/json
+        // solo llegan request que son POST y que tienen el header content-type: application/json
 
-    let body = ''
+        let body = ''
 
-    // escuchar el evento data
-    req.on('data', (chunk) => {
-        body += chunk.toString()
-    })  
+        // escuchar el evento data
+        req.on('data', (chunk) => {
+            body += chunk.toString()
+        })  
 
-    req.on('end', () => {
-        const data = JSON.parse(body)
-        data.timestamp = Date.now()
-        // mutar la request y meter la informacion en el req.body
-        req.body = data
-        next()
+        req.on('end', () => {
+            const data = JSON.parse(body)
+            data.timestamp = Date.now()
+            // mutar la request y meter la informacion en el req.body
+            req.body = data
+            next()
+        })
     })
-})
+*/
 
 app.get('/pokemon/ditto', (req, res) => {
     res.json(ditto)
