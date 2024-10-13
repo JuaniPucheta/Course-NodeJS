4e1309c6141aa06d7393ed63f74efc02c5c7341c
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-10-13 12:16:56 -0300

    feat: done 06

--- 06-chat-tiempo-real/README.md
+++ 06-chat-tiempo-real/README.md
@@ -7,3 +7,15 @@ GET /stylesheets/style.css 304 1.013 ms - -
 GET /favicon.ico 404 1.013 ms - 150
 ```
 La cual dice que se hizo un GET a la ruta / y se respondió con un 304 en 3.013 ms.
+2. Usando turso
+- `wsl`
+- `sudo apt update && sudo apt upgrade -y`
+- `curl -sSfL https://get.tur.so/install.sh | bash`
+- `turso` -> para chequear que se instaló correctamente
+- `turso auth login` -> para loguearse
+- `turso db show calm-malice` -> para ver la base de datos
+- `turso db tokens create calm-malice` -> para crear un token
+- `turso db shell primary-lady-shiva` -> para entrar a la base de datos y hacer queries
+
+> ![IMPORTANT]
+> Pendiente resolver id = null en Turso Drizzle Studio. Correo enviado al soporte de Turso para ver cómo resolverlo.
--- 06-chat-tiempo-real/server/index.js
+++ 06-chat-tiempo-real/server/index.js
@@ -1,29 +1,81 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 
-io.on('connection', (socket) => {
-    console.log('🔗 Nueva conexión')
+const db = createClient({
+    url: "libsql://primary-lady-shiva-juanipucheta.turso.io",
+    authToken: process.env.DB_TOKEN
+})
 
-    socket.on('disconnect', () => {
-        console.log('🔌 Se desconectó')
-    })
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`)
+
+io.on('connection', async (socket) => {
+  console.log('🔗 Nueva conexion')
+
+  socket.on('disconnect', () => {
+    console.log('🔗 Desconectado')
+  })
+
+  socket.on('chat message', async (msg) => {
+    let result
+    const username = socket.handshake.auth.username ?? 'Anónimo'
+    console.log({ username })
+
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+        args: { msg, username }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
+  })
+
+  if (!socket.recovered) { // <- recuperase los mensajes sin conexión
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/client/index.html')
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
 server.listen(port, () => {
-    console.log(`✅ Corriendo en http://localhost:${port}`)
+  console.log(`✅ Corriendo en http://localhost:${port}`)
 })
